c2f0746a1b8d4dec3917e73048c5fc6394f79823
luisjaviermon <42601756+luisjaviermon@users.noreply.github.com>
2019-02-12 16:05:20 -0600

    Add files via upload
    falta revisar dependencias

--- Instalador.sh
+++ Instalador.sh
@@ -3,12 +3,17 @@
 ROJO='\033[01;31m'
 BLANCO='\033[0m'
 CYAN='\033[96m'
-
+VERDE='\033[92m'
+RUTA_TRABAJO="$HOME/Descargas"
+VALIDA=45
 #***********Notas************
+#->para almacenar un retorno de funcion en vez de return usar echo y almacenarla de la siguiente manera #VAR=$(FUNCION)
 :<<: 
+->la maquina que lo ejecutara es el servidor en si 
 
 pagina de ayuda: http://www.w3big.com/es/linux/default.html
 --https://www.vozidea.com/como-descargar-archivos-de-google-drive-desde-la-consola (descarga archivos de drive)
+
 ->para crear variables locales se coloca el modificador "local" antes de declararla
 
 ->Para el paso de argumentos de una funcion se colocan alado y se llaman con la var $num_arg
@@ -19,32 +24,77 @@ pagina de ayuda: http://www.w3big.com/es/linux/default.html
 		*salida: hola mundo este es 212
 
 ->dependencias necesarias
-	-apache ()
+	-apache (no problem con version)
 	-php(5.3 preserente o 5.*)
 	-postgress
-	-ssh (tal vez)
-	-
 :
 
 #------------Pendientes------------
 :<<coment
-
--> implementar para la descarga del comprimido: wget -P $HOME/Documentos https://github.com/luisjaviermon/Asistencia/blob/master/asistencia.zip
+->Revisaar para la instalacion de deéndencias (revisar si estan instaladas o no y preguntar si desea instalarlas)
 
 coment
+function valida_comando {
+	if [ $? -eq 0 ]; then
+		echo 1
+	else
+		echo -1
+	fi
+}
+
+function descomprime {
+	cd $RUTA_TRABAJO
+	local salida
+	unzip asistencia.zip
+	VALIDA=$(valida_comando)
+	if [ $VALIDA -eq 1 ]; then
+		echo -e "\nDescompresion de archivos ${VERDE}[OK]${BLANCO}"
+	else
+		echo -e "\nDescompresion de archivos ${ROJO}[FALLO]${BLANCO}\n------------>Codigo de error: 2"
+	fi
+}
+
+function descarga_archivos {
+	echo "Descargando archivos de intalacion..."
+	cd $RUTA_TRABAJO # cambiarlo por la direccion al sevidor apache
+	if [ ! -f asistencia.zip ]; then
+		wget https://pruebasapifbpr2.000webhostapp.com/asistencia.zip 
+		#wget https://github.com/luisjaviermon/Asistencia/blob/master/asistencia.zip
+		VALIDA=$(valida_comando)
+		if [ $VALIDA -eq 1 ]; then
+			echo -e "\nDescarga de archivos ${VERDE}[OK]${BLANCO}"
+		else
+			echo -e "\nDescarga de archivos ${ROJO}[FALLO]${BLANCO}\nERROR: Falla al descargar archivos\n------------>Codigo de error: 1"
+		fi
+	else
+		echo -e "\nDescarga de archivos ${VERDE}[OK]${BLANCO}"
+	fi
+}
 
 function sudo_corr {
-	local x=$2
+	echo "prueba"
+	VALIDA=$(valida_comando)
+	if [ ! $VALIDA -eq 1 ]; then
+		exit 0
+	fi
+	descarga_archivos
+	if [ ! $VALIDA -eq 1 ]; then
+		exit 1
+	fi
+	descomprime
+	if [ ! $VALIDA -eq 1 ]; then
+		exit 2
+	fi
+	
 }
 
 function sudo_err {
 	echo -e "\n${ROJO}Para ejecutar este instalador porfavor ejecutelo de la siguiente manera: \n\n\t${CYAN}sudo ./instalador.sh${ROJO}${BLANCO}\n"
-	printf "hola"
 }
 
 function main {
 	if [ $UID != 0 ]; then #Verificar si se ejecuta como superusuario *NOTA:cambiar != por ==
-		sudo_corr "hola mundo" 212
+		sudo_corr 
 	else
 		sudo_err
 	fi
